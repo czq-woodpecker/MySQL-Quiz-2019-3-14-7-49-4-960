20122f3ed5a66d801964e1bdf38282614bfb60c3
zeqiang.chen <1209120868@qq.com>
2019-07-29 21:20:02 +0800

    [zeqiang] feat: add the SQL of practice2

--- practice2.sql
+++ practice2.sql
@@ -1,2 +1,12 @@
 # 取得每个company中最高薪水的人员名字
 # 输出结果包含公司名称和人员名称：companyName name
+
+select e1.name, c.companyName
+from employee e1
+inner join company c on e1.companyId = c.id,
+(select name, max(salary) from employee group by companyId) e2
+where e1.name = e2.name;
+
+xiaoming  baidu
+xiaohong  alibaba
+xiaozhi   tengxun
