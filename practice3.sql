68166f70816c2f6745db607614c0226d5206c572
zeqiang.chen <1209120868@qq.com>
2019-07-29 21:59:09 +0800

    [zeqiang] feat: add the SQL of practice3

--- practice3.sql
+++ practice3.sql
@@ -1,4 +1,9 @@
 #取得平均薪水最高的公司
 #输出公司名称和平均薪水：companyName avgSalary
 
+select c.companyName, avg(e.salary) as avgSalary
+from employee e
+inner join company c on e.companyId = c.id
+group by c.id order by avgSalary desc limit 1;
 
+tengxun  8000.0000
